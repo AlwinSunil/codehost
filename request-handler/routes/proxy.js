a391b0d38b1344d614dcabe53b928461e3473223
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2024-09-06 16:36:40 +0530

    Updated request-handler service to cache subdomain-to-projectid mappings

--- request-handler/routes/proxy.js
+++ request-handler/routes/proxy.js
@@ -1,21 +1,47 @@
 import httpProxy from "http-proxy";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+	url: process.env.UPSTASH_REDIS_URL,
+	token: process.env.UPSTASH_REDIS_TOKEN,
+});
 
 const BASE_PATH =
-	"https://codehost-test.s3.eu-north-1.amazonaws.com/_deployments";
+	"https://codehost-prod.s3.ap-south-1.amazonaws.com/_deployments";
 const proxy = httpProxy.createProxyServer();
 
 const getTargetUrl = async (prisma, subdomain) => {
 	try {
+		const cachedProjectId = await redis.get(subdomain);
+
+		if (cachedProjectId) {
+			console.log("From cache:", cachedProjectId);
+			return `${BASE_PATH}/${cachedProjectId}/`;
+		}
+
 		const project = await prisma.project.findUnique({
 			where: { subdomain },
+			select: {
+				id: true,
+				subdomain: true,
+			},
 		});
-		return project ? `${BASE_PATH}/${project.id}/` : null;
+		console.log("From DB:", project?.id);
+
+		if (project) {
+			await redis.set(subdomain, project.id, { ex: 172800 });
+
+			return `${BASE_PATH}/${project.id}/`;
+		}
+
+		return null;
 	} catch (err) {
-		console.error("Database error:", err);
+		console.error("Error fetching target URL:", err);
 		return null;
 	}
 };
 
+// Proxy route handler
 const proxyRoutes = (prisma) => {
 	return async (req, res, next) => {
 		const subdomain = req.hostname.split(".")[0];
@@ -31,12 +57,12 @@ const proxyRoutes = (prisma) => {
 				(err) => {
 					if (err) {
 						console.error("Proxy error:", err);
-						next(); // Move to next middleware or route
+						next();
 					}
 				}
 			);
 		} else {
-			next(); // Move to next middleware or route
+			next();
 		}
 	};
 };
