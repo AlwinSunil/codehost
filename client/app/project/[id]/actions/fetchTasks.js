832b8e8de20e76311ed9bdff9d65bd1e9480b3a0
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2024-10-07 14:37:10 +0530

    Fixed bug in task table in project page

--- client/app/project/[id]/actions/fetchTasks.js
+++ client/app/project/[id]/actions/fetchTasks.js
@@ -1,17 +1,23 @@
 "use server";
 
+import { getServerSession } from "next-auth";
+
+import { authConfig } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 
 export async function fetchTasks(projectId, skip = 0, take = 10, userId) {
+  const session = await getServerSession(authConfig);
+  if (!session || !session.user) {
+    return { success: false, error: "User not authenticated" };
+  }
+
   const tasks = await prisma.task.findMany({
     where: {
       projectId,
       userId,
-      OngoingJob: { isNot: null },
     },
     include: {
       OngoingJob: true,
-      user: true,
     },
     orderBy: [{ startedAt: "desc" }, { lastUpdated: "desc" }],
     skip: parseInt(skip),
